5fd4b0c5e49a4a9ddc78af4bf6a64b9b9527853e
Edmund von der Burg <evdb@ecclestoad.co.uk>
2011-01-13 12:21:36 -0300

    Debug only on dev server

--- teamroom_app/main.py
+++ teamroom_app/main.py
@@ -1,6 +1,7 @@
+import os
+
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
-from google.appengine.ext.webapp import template
 
 from teamroom import holding
 
@@ -9,11 +10,13 @@ def main():
         [
             ('/', holding.Handler),
         ],
-        debug=True
+        debug = on_dev_server()
     )
     
     util.run_wsgi_app(application)
 
+def on_dev_server():
+    os.environ['SERVER_SOFTWARE'].startswith('Dev')
 
 if __name__ == '__main__':
     main()
